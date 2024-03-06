24c908dd9b715d982e326027749e9ad71d76be79
zack <6351754+zackkrida@users.noreply.github.com>
2024-03-06 09:43:31 -0500

    Update card.js

--- bin/card.js
+++ bin/card.js
@@ -13,17 +13,27 @@ const options = {
 const data = {
   name: chalk.white('Zack Krida /'),
   handle: chalk.cyan('@zackkrida'),
-  work: chalk.white('Fullstack freelance developer'),
-  twitter: chalk.cyan('https://twitter.com/zackkrida'),
+  work: chalk.white('Fullstack freelance#!/usr/bin/env node
+
+const chalk = require('chalk')
+const boxen = require('boxen')
+
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round',
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('Zack Krida /'),
+  handle: chalk.cyan('@zackkrida'),
+  work: chalk.white('Building Openverse at WordPress'),
+  sponsor: chalk.white('Sponsored by Automattic'),
   github: chalk.cyan('https://github.com/zackkrida'),
-  linkedin: chalk.cyan('https://linkedin.com/in/zackkrida'),
-  web: chalk.cyan('https://zackkrida.com'),
   npx: chalk.white('npx zackkrida'),
   labelWork: chalk.white.bold('      Work:'),
-  labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  labelWeb: chalk.white.bold('       Web:'),
   labelCard: chalk.white.bold('      Card:'),
 }
 
@@ -31,10 +41,7 @@ const data = {
 const newline = '\n'
 const heading = `${data.name} ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
@@ -44,13 +51,7 @@ const output =
   newline +
   working +
   newline +
-  twittering +
-  newline +
   githubing +
-  newline +
-  linkedining +
-  newline +
-  webing +
   newline +
   newline +
   carding
